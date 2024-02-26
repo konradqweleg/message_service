05de643496f9fd48cb453ab6ace70796ffa610d7
Konrad Groń <konradnrog@gmail.com>
2024-02-26 20:56:07 +0100

    first commit

---
+++ src/main/java/com/example/message_history_service/MessageHistoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.message_history_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageHistoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageHistoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/message_history_service/MessageHistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.message_history_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageHistoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
